37ceb5a1a221fd3993b8c1dd55b8e54848c37537
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-06-02 12:45:17 -0300

    chore: Add OTPInputProps interface for OTP component

---
+++ src/components/OTP/OTP.types.ts
@@ -0,0 +1,53 @@
+import { InputProps } from "@nextui-org/input";
+
+import { HeightClass } from "./HeightClass.enum";
+import { SpacingClass } from "./SpacingClass.enum";
+import { WidthClass } from "./WidthClass.enum";
+
+export type OtpType = {
+  otp: string;
+};
+
+export type AcceptType = "numbers" | "letters" | "alphanumeric";
+
+export type OTPInputProps = {
+  /**
+   * Number of characters accepted by the OTP
+   * @default "6"
+   */
+  length?: number;
+
+  /**
+   * Callback function called when the OTP is filled
+   */
+  onComplete?: (otp: string) => void;
+
+  /**
+   * Accepted by OTP, numbers, letters or alphanumeric
+   * @default "numbers"
+   */
+  accept?: AcceptType;
+
+  /**
+   * Props for the input component
+   */
+  inputProps?: InputProps;
+
+  /**
+   * Define the height of the input.
+   * Refer to HeightClass for possible values and their corresponding sizes.
+   */
+  inputHeight?: keyof typeof HeightClass;
+
+  /**
+   * Define the width of the input.
+   * Refer to WidthClass for possible values and their corresponding sizes.
+   */
+  inputWidth?: keyof typeof WidthClass;
+
+  /**
+   * Define the spacing between the input.
+   * Refer to SpacingClass for possible values and their corresponding sizes.
+   */
+  spacing?: keyof typeof SpacingClass;
+};
