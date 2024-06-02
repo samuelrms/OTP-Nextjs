2f1141f750443f7262edca07a2a4ffffdee6adaf
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-06-02 15:00:00 -0300

    chore: Update OTP component usage examples for React and Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import { OTP } from "$/components";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <OTP length={6} onComplete={(c) => console.log(c)} />
+      <OTP
+        length={6}
+        onComplete={(c) => console.log(c)}
+        inputHeight="h-10"
+        // inputWidth="w-10"
+      />
     </main>
   );
 }
